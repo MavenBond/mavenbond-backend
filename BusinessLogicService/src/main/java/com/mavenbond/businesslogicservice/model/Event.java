b462f770bbb56e4a9177affd11548a90c611e09e
khangnguyen111101 <khangnguyen111101@gmail.com>
2022-12-25 07:18:23 +0700

    1. Create Business Logic Service, Dockerized

---
+++ BusinessLogicService/src/main/java/com/mavenbond/businesslogicservice/model/Event.java
@@ -0,0 +1,5 @@
+package src.main.java.com.mavenbond.businesslogicservice.model;
+import javax.persistence.*;
+
+public class Event {
+}
---
+++ BusinessLogicService/src/main/java/com/mavenbond/businesslogicservice/model/Offer.java
@@ -0,0 +1,4 @@
+package src.main.java.com.mavenbond.businesslogicservice.model;
+
+public class Offer {
+}
