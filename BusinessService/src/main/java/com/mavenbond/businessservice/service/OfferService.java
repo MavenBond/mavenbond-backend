f1929d8f0e5f17c8ef90b48691e4c030a4b2ec37
khangnguyen111101 <khangnguyen111101@gmail.com>
2023-01-09 17:39:12 +0700

    Update Service

--- BusinessService/src/main/java/com/mavenbond/businessservice/service/OfferService.java
+++ BusinessService/src/main/java/com/mavenbond/businessservice/service/OfferService.java
@@ -1,13 +1,23 @@
 package com.mavenbond.businessservice.service;
 
 import com.mavenbond.businessservice.model.Offer;
+import com.mavenbond.businessservice.repository.OfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface OfferService {
-    List<Offer> findAll();
-    void save(Offer offer);
-    void delete(Long offerId);
-    Optional<Offer> findById(Long id);
+@Service
+public class OfferService {
+    @Autowired
+    private OfferRepository repository;
+
+    public List<Offer> findAll() { return repository.findAll(); }
+
+    public void save(Offer offer) { repository.save(offer); }
+
+    public void delete(Long offerId) { repository.deleteById(offerId); }
+
+    public Optional<Offer> findById(Long id) { return repository.findById(id); }
 }
